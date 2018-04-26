15080ffbec48427dd0fb97a02211d2647402df07
antoniogamiz <antoniogamiz10@gmail.com>
2018-04-26 17:47:00 +0200

    Bug fix

--- deepspace/src/deepspace/EnemyStarShip.java
+++ deepspace/src/deepspace/EnemyStarShip.java
@@ -22,7 +22,7 @@ public class EnemyStarShip implements SpaceFighter{
         name = n;
         ammoPower = a;
         shieldPower = s;
-        loot = new Loot(l.getNSupplies(), l.getNWeapons(), l.getNShields(), l.getNHangars(), l.getNMedals());
+        loot = new Loot(l.getNSupplies(), l.getNWeapons(), l.getNShields(), l.getNHangars(), l.getNMedals(), false, false);
         damage = d;
     }
     
