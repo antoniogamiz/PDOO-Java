68bdbbfb9825abf6a2c4442a1e2c05f15b192850
antoniogamiz <antoniogamiz10@gmail.com>
2018-05-13 09:51:03 +0200

    cambiado segundo constructor de loot a protected

--- deepspace/src/PruebasP4/Deepspace.java
+++ deepspace/src/PruebasP4/Deepspace.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package PruebasP4;
+import java.util.ArrayList;
 
+import deepspace.*;
 /**
  *
  * @author antonio
@@ -12,5 +14,16 @@ package PruebasP4;
 public class Deepspace {
     public static void main( String[] args ) {
         System.out.println("Programa de pruebas para P4");
+
+        //Inicializamos GameUniverse
+        GameUniverse game = new GameUniverse();
+        ArrayList<String> names = new ArrayList();
+        names.add("Antonio"); names.add("Antonio2");
+        game.init(names);
+        
+        //nSupplies, nWeapons, nShields, nHangars, nMedals, [ef, city]
+        //Loot loot = new Loot( 0, 1, 2, 3, 4); constructor protected
+        
+
     }
 }
